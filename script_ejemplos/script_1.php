7374a4194719963b7e0689584289fdf9355f663f
john <johnalexguerrero@gmail.com>
2019-03-14 21:50:06 -0500

    script php

---
+++ script_ejemplos/script_1.php
@@ -0,0 +1,9 @@
+<center>Mi script </center>
+<?php
+    //crear variable
+    $nombre = "joan";
+    $apellido = "ayala";
+    $email = "joanAyalajoan@hotmail.com";
+
+    echo "wellcome "."<br>".$nombre;
+?>
